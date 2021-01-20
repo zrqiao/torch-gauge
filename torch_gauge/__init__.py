d915790cf4d58da276517514141703fc6e625fec
Zhuoran Qiao <zqiao@caltech.edu>
2021-01-19 21:35:05 -0800

    Versioneer

--- torch_gauge/__init__.py
+++ torch_gauge/__init__.py
@@ -4,3 +4,7 @@ from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
