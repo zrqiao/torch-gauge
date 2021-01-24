745a6a9c5d05376cf4e2f0eb10f02eed8443224c
Zhuoran Qiao <zqiao@caltech.edu>
2021-01-24 11:27:51 -0800

    Test Levi-Civita

--- torch_gauge/tests/test_clebsch_gordan.py
+++ torch_gauge/tests/test_clebsch_gordan.py
@@ -1,4 +1,10 @@
-from torch_gauge.o3.clebsch_gordan import get_clebsch_gordan_coefficient
+import torch
+
+from torch_gauge.o3.clebsch_gordan import (
+    LeviCivitaCoupler,
+    get_clebsch_gordan_coefficient,
+)
+from torch_gauge.o3.spherical import SphericalTensor
 
 
 def test_generate_cg():
@@ -12,3 +18,17 @@ def test_generate_cg():
                 for m1 in range(-j1, j1 + 1):
                     for m2 in range(-j2, j2 + 1):
                         get_clebsch_gordan_coefficient(j1, j2, j, m1, m2, m1 + m2)
+
+
+def test_levi_civita():
+    metadata = torch.LongTensor([[24, 12]])
+    dten1 = torch.rand(4, 6, 2, 60, 7)
+    dten2 = torch.rand(4, 6, 2, 60, 7)
+    spten1 = SphericalTensor(dten1, (3,), metadata)
+    spten2 = SphericalTensor(dten2, (3,), metadata)
+    coupler = LeviCivitaCoupler(metadata[0])
+    overlap_out = coupler(spten1, spten2, overlap_out=True)
+    assert overlap_out.ten.shape == (4, 6, 2, 60, 7)
+    cat_out = coupler(spten1, spten2, overlap_out=False)
+    assert cat_out.ten.shape == (4, 6, 2, 144, 7)
+    assert torch.all(cat_out.metadata.eq(torch.LongTensor([[36, 36]])))
