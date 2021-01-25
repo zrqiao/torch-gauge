58977c6a18c421742c6a9f9eacf9250ca4e8951e
Zhuoran Qiao <zqiao@caltech.edu>
2021-01-24 17:09:55 -0800

    Add readme to description

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 import versioneer
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="torch_gauge",
     description="A light-weight PyTorch extension for gauge-equivariant geometric learning",
